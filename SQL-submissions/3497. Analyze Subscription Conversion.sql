5410a0ca9585be55670b180df53dd677cbffe759
zandratay <88842830+zandratay@users.noreply.github.com>
2025-04-01 16:07:00 +0800

    Create 3497. Analyze Subscription Conversion.sql

---
+++ SQL-submissions/3497. Analyze Subscription Conversion.sql
@@ -0,0 +1,22 @@
+-- BEATS 65%
+
+WITH paid_users AS (
+    SELECT
+        DISTINCT user_id
+    FROM UserActivity u1
+    WHERE EXISTS (
+        SELECT 1 FROM UserActivity u2
+        WHERE u1.user_id = u2.user_id
+        AND u2.activity_type IN ('free_trail', 'paid')
+    )
+)
+
+SELECT 
+    pu.user_id, 
+    ROUND(SUM(IF(ua.activity_type = 'free_trial', activity_duration, 0)) / SUM(IF(ua.activity_type = 'free_trial', 1, 0)), 2) AS trial_avg_duration,
+    ROUND(SUM(IF(ua.activity_type = 'paid', activity_duration, 0)) / SUM(IF(ua.activity_type = 'paid', 1, 0)), 2) AS paid_avg_duration
+FROM paid_users pu
+LEFT JOIN UserActivity ua
+ON pu.user_id = ua.user_id
+GROUP BY pu.user_id
+ORDER BY pu.user_id 
