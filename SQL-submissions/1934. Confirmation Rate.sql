ba9aa781669ef28000e25c9059df062864d43688
zandratay <88842830+zandratay@users.noreply.github.com>
2025-04-04 17:56:38 +0800

    Update 1934. Confirmation Rate.sql

--- SQL-submissions/1934. Confirmation Rate.sql
+++ SQL-submissions/1934. Confirmation Rate.sql
@@ -1,16 +1,15 @@
--- BEATS 80.06%
+-- BEATS 95.59%
 
-# Write your MySQL query statement below
 
-WITH tbl AS (
-    SELECT s.user_id,
-    c.action
+WITH joined AS (
+    SELECT s.user_id, c.time_stamp, c.action
     FROM Signups s
     LEFT JOIN Confirmations c
     ON s.user_id = c.user_id
 )
+
 SELECT 
     user_id,
-    COALESCE(ROUND(sum(CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(*), 2) , 0) AS confirmation_rate
-FROM tbl
+    ROUND(COALESCE(SUM(IF(action = 'confirmed', 1, 0)) / COUNT(*), 0), 2) AS confirmation_rate
+FROM joined
 GROUP BY user_id
